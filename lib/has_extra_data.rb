cb16b19b528474d81d8579962418d7cc1216713a
Jeremy Walker <jez.walker@gmail.com>
2011-05-16 16:02:09 +0100

    Removed test exception

--- lib/has_extra_data.rb
+++ lib/has_extra_data.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext'
-raise File.exists?(File.join(File.dirname(__FILE__), "has_extra_data/railtie.rb")).to_s
 require File.join(File.dirname(__FILE__), "has_extra_data/railtie.rb")
 
 #module HasExtraData
