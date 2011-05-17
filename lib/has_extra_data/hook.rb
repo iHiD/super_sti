f795ce2a83c51200f4dc5db503d843536f60d588
Jeremy Walker <jez.walker@gmail.com>
2011-05-17 01:30:52 +0100

    Removed attributes= method and replaced it with respond_to? override.

--- lib/has_extra_data/hook.rb
+++ lib/has_extra_data/hook.rb
@@ -17,60 +17,33 @@ module HasExtraData
 
       table_name = options[:table_name] || "#{self.name.underscore.gsub("/", "_")}_data"
       klass = Class.new(ActiveRecord::Base) do
-      
         set_table_name(table_name)
-        
-        #def self.parent_klass=(klass)
-        #  @parent_klass = klass
-        #end
-
-        def self.belongs_to(sym, *args)
-          super(sym, *args)
-          #@parent_klass.has_one sym, :through => :data
-        end
       end
-      #klass.parent_klass = self
       klass.class_eval &block if block_given?
       self.const_set "Data", klass
   
+      # Add a reference to a data object that gets created when this is created
       has_one :data, :class_name => "#{self.name}::Data"
       before_create :get_data
       
+      # A helper method which gets the existing data or builds a new object
       define_method :get_data do
         data || build_data
       end
       
-      define_method "attributes=" do |new_attributes, guard_protected_attributes = true|
-        get_data.attribute_names.each do |attr_name|
-          unless respond_to? attr_name
-            self.class.send(:define_method, attr_name) do
-              data.send(attr_name)
-            end
-          end
-          unless respond_to? "#{attr_name}="
-            self.class.send(:define_method, "#{attr_name}=") do |val|
-              data.send("#{attr_name}=", val)
-            end
-          end
-        end
-        super(new_attributes, guard_protected_attributes)
+      # Override respond_to? to check both this object and its data object.
+      define_method "respond_to?" do |sym, include_private = false|
+        super(sym, include_private) || get_data.respond_to?(sym, include_private)
       end
       
+      # Override method_missing to check both this object and it's data object for any methods or magic functionality.
+      # Firstly, try the original method_missing because there may well be 
+      # some magic piping that will return a result and then try the data object.
       define_method :method_missing do |sym, *args|
-      
-        # Firstly, try the original method_missing because there may well be 
-        # some magic piping that will return a result.
         begin
           super(sym, args)
         rescue
-          
-          # Do we want to raise the initial exception, for a cleaner stack trace?
-          #exception = $!
-          #begin
-            get_data.send(sym, *args)
-          #rescue
-          #  raise exception
-          #end
+          get_data.send(sym, *args)
         end
       end
     end
